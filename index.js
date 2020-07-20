4197873d5eb37b5666c4779ac4f73e5aec6ba814
Chelsea Torres <67041747+chelstorres13@users.noreply.github.com>
2020-07-19 22:06:51 -0500

    initial upload
    html & css from Thinkful. js file is my own.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shoppingListCheck(){
+//add items typed in
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    let shoppingItemName = $('#shopping-list-entry').val();
+  $('.shopping-list').prepend(`<li>
+        <span class="shopping-item">${shoppingItemName}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`)
+
+  });
+    //toggle between check and uncheck
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {$(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+   });
+  //remove items
+  $('.shopping-item-controls').on('click', '.shopping-item-delete', function(event) {$(this).closest('li').remove();
+   });
+
+}
+$(shoppingListCheck());
